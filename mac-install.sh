b30856d5e862ef7674d90f5fc85ceef315b527b5
xndi <azkfv@protonmail.com>
2025-03-31 10:20:21 -0300

    update commands

--- mac-install.sh
+++ mac-install.sh
@@ -4,9 +4,9 @@
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock magnification -float 1.0
 defaults write com.apple.dock mineffect -string "genie"
-# dock instant show without delay
-defaults write com.apple.dock autohide-delay -float 0 && killall Dock
-
+# dock instant show without much delay
+defaults write com.apple.dock autohide-time-modifier -float 0.7
+killall Dock
 # finder settings
 defaults write com.apple.finder ShowStatusBar -bool true
 defaults write com.apple.finder ShowPathbar -bool true
@@ -15,8 +15,11 @@ defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.controlcenter "NSStatusItem Visible Sound" -bool true
 defaults write com.apple.controlcenter "NSStatusItem Visible Battery" -bool true
 
-defaults write -g KeyRepeat -int 1
-defaults write -g InitialKeyRepeat -int 10
-
-# vim goodies
+# allow key repeat
+defaults write -g KeyRepeat -int 1.9
+defaults write -g InitialKeyRepeat -int 14
+# disable press and hold
 defaults delete -g ApplePressAndHoldEnabled
+
+# really quit finder when cmd+q
+defaults write com.apple.finder QuitMenuItem -bool YES
